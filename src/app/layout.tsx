f698118d0c92ae98eb2eae0fd2fe953d73fedfaa
sid111rawal <sid111rawal@gmail.com>
2026-08-13 09:53:53 -0300

    fix: dynamically generate valid 48x48 favicon for Google Search indexing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
